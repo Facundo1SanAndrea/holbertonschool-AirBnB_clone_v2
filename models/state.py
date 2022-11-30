de420a6a997d460522782ddaa5f835547bf4cd4f
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-11-30 14:11:11 -0300

    fixing state

--- models/state.py
+++ models/state.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer
+from models.base_model import BaseModel
+from models.base_model import Base
+from models.city import City
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import models
-import os
-STRG = os.environ.get('HBNB_TYPE_STORAGE')
 
 
 class State(BaseModel, Base):
     """ State class """
-    if STRG == 'db':
-        __tablename__ = "states"
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state",
-                              cascade="all, delete, delete-orphan")
-    else:
-        name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade='delete')
 
-        @property
-        def cities(self):
-            """ Getter method """
-            from models import storage
-            my_list = []
-            for i in list(storage.all(City).values()):
-                if self.id == i.state_id:
-                    my_list.append(i)
-            return my_list
+    @property
+    def cities(self):
+        """Returns list of city instances in same state"""
+        from models import storage
+        my_list = []
+        for i in list(storage.all(City).values()):
+            if self.id == i.state_id:
+                my_list.append(i)
+        return my_list
